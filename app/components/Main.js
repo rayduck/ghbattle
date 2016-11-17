5b5bffc1d58c700553370ab19fbdc36349315ea0
rayduck <ruida@dingansi.ch>
2016-11-18 00:17:49 +0800

    some changes to main component and adds hashHistory

--- app/components/Main.js
+++ app/components/Main.js
@@ -3,11 +3,12 @@ var React= require('react');
 var Main = React.createClass({
   render: function(){
     return (
-      <div> Hello from Main!
+      <div className='main-container'>
         {this.props.children}
       </div>
     )
   }
 });
+//class is reserved so use className
 
 module.exports = Main;
--- app/config/routes.js
+++ app/config/routes.js
@@ -3,13 +3,14 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
+var hasHistory = ReactRouter.hashHistory;
 var Main = require('../components/Main');
 var Home = require('../components/Home');
 
 var routes = (
-  <Router>
+  <Router history={hashHistory}>
     <Route path='/' component={Main}>
-      <Route path='/home' component={Home} />
+      <IndexRoute path='/home' component={Home} />
     </Route>
   </Router>
 );
