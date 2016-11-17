22f923f0cb71d0a682077eaeee4df9b900fff0d8
rayduck <ruida@dingansi.ch>
2016-11-17 14:06:28 +0800

    adds Profile Avatar

--- app/index.js
+++ app/index.js
@@ -1,15 +1,61 @@
+/*
+  Focuses
+  Independent
+  Reusable
+  Small
+  Testable
+*/
+
+var USER_DATA= {
+  name: 'Meme man',
+  username: 'rayduck',
+  image: 'http://d.hiphotos.baidu.com/zhidao/wh%3D450%2C600/sign=a961599df1d3572c66b794d8bf234f11/0824ab18972bd407964add8978899e510fb30930.jpg'
+};
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+// f(d)=v
+
+var ProfilePic = React.createClass({
+  render: function(){
+    return <img src={this.props.imageUrl} style={{height:100, width:100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
   render: function(){
     return (
-      <div> Hello World </div>
+      <div>
+      <a href={'https://www.github.com/' + this.props.username}>
+        {this.props.username}
+        </a>
+        </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function(){
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
     )
   }
 });
 
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA}/>,
   document.getElementById('app')
 );
